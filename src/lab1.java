206a7df285a75a394b8975abeda3c50a3d6e7ab1
Artem <artempozitiv56@gmail.com>
2022-10-04 13:41:33 +0200

    lab1

---
+++ src/lab1.java
@@ -0,0 +1,28 @@
+ public class lab1 {
+     public static void main(String[] args){
+         final double m = 7;
+         final double n = 10;
+
+         final int a = 4;
+         final int b = 3;
+         final int c = 1;
+
+         double sum = 0;
+         double denominator = 0;
+         double numerator = 0;
+
+         for (double i = a; i <= n; i++){
+             denominator = (i + c);
+             for (double j = b; j <= m; j++){
+                 numerator = (i + j);
+                 if (denominator != 0) {
+                     sum += numerator / denominator;
+                 } else {
+                     System.out.println("Division by 0!");
+                     return;
+                 }
+             }
+         }
+         System.out.println("Result: " + sum);
+     }
+ }
